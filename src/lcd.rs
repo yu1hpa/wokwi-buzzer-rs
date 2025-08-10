001bde098b68081e76671b803db357a5ae7220ca
yu1hpa <yu.1hpa@gmail.com>
2025-08-10 20:54:30 +0900

    feat: hd44780-driverを最新のコミットのものにして`hal::pwm`を使えるようにした

--- src/lcd.rs
+++ src/lcd.rs
@@ -1,5 +1,10 @@
-use hd44780_driver::bus::I2CBus;
-use hd44780_driver::HD44780;
+use hd44780_driver::{
+    bus::I2CBus,
+    charset::{CharsetUniversal, Fallback},
+    memory_map::MemoryMap1602,
+    setup::DisplayOptionsI2C,
+    HD44780,
+};
 use rp_pico::hal::i2c::{ValidPinScl, ValidPinSda, I2C};
 use rp_pico::hal::pac::I2C0;
 use rp_pico::hal::Timer;
@@ -8,6 +13,7 @@ const LCD_ADDR: u8 = 0x27;
 
 #[derive(Debug)]
 pub enum LcdError {
+    InitError,
     ClearError,
     WriteStrError,
 }
@@ -17,7 +23,7 @@ where
     SDA: ValidPinSda<I2C0>,
     SCL: ValidPinScl<I2C0>,
 {
-    lcd: HD44780<I2CBus<I2C<I2C0, (SDA, SCL)>>>,
+    lcd: HD44780<I2CBus<I2C<I2C0, (SDA, SCL)>>, MemoryMap1602, Fallback<CharsetUniversal, 32>>,
 }
 
 impl<SDA, SCL> LcdDisplay<SDA, SCL>
@@ -25,11 +31,16 @@ where
     SDA: ValidPinSda<I2C0>,
     SCL: ValidPinScl<I2C0>,
 {
-    pub fn new(i2c: I2C<I2C0, (SDA, SCL)>, mut timer: Timer) -> Self {
-        let mut lcd = HD44780::new_i2c(i2c, LCD_ADDR, &mut timer)
-            .unwrap_or_else(|_| core::panic!("LCD init error!"));
-        lcd.clear(&mut timer).unwrap();
-        Self { lcd }
+    pub fn new(i2c: I2C<I2C0, (SDA, SCL)>, mut timer: Timer) -> Result<Self, LcdError> {
+        let options = DisplayOptionsI2C::new(MemoryMap1602::new()).with_i2c_bus(i2c, LCD_ADDR);
+        let mut lcd = match HD44780::new(options, &mut timer) {
+            Ok(display) => display,
+            Err(_) => return Err(LcdError::InitError),
+        };
+        if let Err(_) = lcd.clear(&mut timer) {
+            return Err(LcdError::ClearError);
+        }
+        Ok(Self { lcd })
     }
 
     pub fn write_line(&mut self, text: &str, timer: &mut Timer) -> Result<(), LcdError> {
